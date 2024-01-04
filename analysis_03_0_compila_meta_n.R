7fd5f4e7993602d157e6c1b6a391a6b27dfd6afa
Mateus Figueiredo <mateus.silvafigueiredo@gmail.com>
2024-01-03 23:16:32 -0300

    Upload comparação das notas
    Upload de analysis_03_0_compila_meta, que justapõe as notas máxima, média e mínima de cada concorrência. Upload de analysis_03_1_compara_conc_notas que trata esses dados, fazendo todas as concorrências em relação a c1, e ainda exporta como Excel colorido conforme formatação condicional.

---
+++ analysis_03_0_compila_meta_n.R
@@ -0,0 +1,144 @@
+# # ==============================================================================
+# Arquivo: analysis_03_0_compila_meta_n.R
+
+# Compila dados dos cinco modelos de concorrencia:
+# 1 = concorrencia separada (aka listas multiplas)
+# 2 = concorrencia concomitante, AC primeiro
+# 3 = concorrencia concomitante, cotas primeiro
+# 4 = concorrencia segundo Bó e Senkevics, 2023
+# 5 = concorrencia adaptado de Bó e Senkevics, 2023
+
+# Faz:
+# dfs meta_n_ CURSO _ SISU ANO
+# Atribuir NA para linhas com Nan, -Inf ou Inf
+
+# Falta:
+# Exportar como Word (talvez fazer em outro script)
+
+# Objetivo final:
+# Gerar Word compila_meta_n
+
+# Modificado em 2024-01-03.
+# Autor: Mateus Silva Figueiredo
+
+# ==============================================================================
+# Carregar dados
+
+# Carregar dados com data_04_carregar_dados_UFV.R
+por_curso <- T   # deseja separar candidatos por curso? obrigatório
+por_ano   <- F   # deseja separar candidatos por ano? opcional
+if (!exists("dados_ZOOTECNIA")) source("data_04_carregar_dados_UFV.R") # cria ~80 objetos
+
+# Criar vetores n_vagas para cada curso, com base no termo de adesão de 2022
+setwd("C:/Users/Mateus/Desktop/R/alocacao_vagas_cotas")
+if (!exists("vagas_MEDICINA")) source("data_05_carregar_termo_adesao.R") # cria ~70 objetos
+
+# Carregar df_so_concorridos
+if (!exists ("df_so_concorridos")) source("analysis_001_compara_ins_vagas.R")
+
+# criar vetor mod, com a ordem das modalidades
+mod <- c("A0","L01","L02","L05","L06", "L09", "L10", "L13", "L14")
+# ------------------------------------------------------------------------------
+# Preparação
+# options(digits = 8) # talvez melhor não
+library(tidyverse)
+
+# ==============================================================================
+# Iniciar alocação
+# definir inicio e fim, para depois rodar for loop
+
+# para um conjunto
+#i<-1
+# ------------------------------------------------------------------------------
+
+# apenas alguns conjuntos
+# inicio<-29; fim<-31
+
+# ------------------------------------------------------------------------------
+# ================ ABRE FOR LOOP ===============================================
+
+# para todos os conjuntos:
+inicio<-1; fim<-nrow(df_so_concorridos)
+
+for (i in inicio:fim){ # abre loop i, um para cada conjunto
+  
+  # gerar conjunto candidatos a partir de número 1 a 63
+  candidatos <- dados_ufv[Curso==df_so_concorridos[i,1]][Processo_Seletivo==df_so_concorridos[i,2]]
+  
+  if(exists("nvagas")) rm("nvagas")
+  # Atribuir nvagas correto # criar nvagas a partir do curso em candidatos
+  nvagas <- get(paste0("nvagas_",candidatos$Curso[1]))
+  nvagas
+  
+  # para regularizar, fingir que nenhum candidato foi convocado ainda
+  candidatos$mod_con <- 0
+  candidatos %>% head
+  
+  # remover objetos analise_n e analise_v
+  rm(list = ls(pattern = "^analise_n"))
+  rm(list = ls(pattern = "^analise_v"))
+  
+  setwd("C:/Users/Mateus/Desktop/R/alocacao_vagas_cotas") # definir pasta
+  if (!exists ("analise_v_c5")){ 
+    # Correr concorrências
+    source("analysis_01_1_conc_separada.R") # parece ok
+    source("analysis_01_2_conc_c_ac.R")     # parece ok 
+    source("analysis_01_3_conc_c_cotas.R")  # parece ok           
+    source("analysis_01_4_conc_bo.R")       # parece ok   
+    source("analysis_01_5_conc_ob.R")       # testando           
+    
+  }
+  # ==============================================================================
+  # Criar df meta_n com todas as notas dos tres modelos
+  meta_n<-rbind(analise_n_c1,analise_n_c2,analise_n_c3,analise_n_c4,analise_n_c5)
+  
+  # ==============================================================================
+  
+  
+  # ------------------------------------------------------------------------------
+  # verificar resultados
+  print(meta_n)
+  print(paste(candidatos$Curso[1],candidatos$Processo_Seletivo[1]))
+  print(paste(sum(nvagas),"== sum(nvagas)"))
+
+  # ------------------------------------------------------------------------------
+  # salvar como meta_n_ curso _ sisu, usa como fonte candidatos
+  eval(parse(text=(paste0(
+    "meta_n_",candidatos$Curso[1],"_",candidatos$Processo_Seletivo[1]," <- meta_n"
+  ))))
+  # 
+} # fim do loop i, de cada conjunto
+
+# ==============================================================================
+# Limpeza
+if(T){
+  # remover objetos analise_n e analise_v
+  rm(list = ls(pattern = "^comp_vagas"))
+  rm(list = ls(pattern = "^convocados"))
+  rm("candidatos")
+}
+
+# ==============================================================================
+
+# Se a linha tiver NaN, Inf ou -Inf, faça a linha inteira virar NA
+
+# ------------- ABRE FOR LOOP --------------------------------------------------
+
+# para todos os conjuntos:
+inicio<-1; fim<-nrow(df_so_concorridos)
+
+for (i in inicio:fim){ # abre loop
+  
+# df <- df_meta_n_ CURSO ANO 
+eval(parse(text=(paste0(
+  "df <- meta_n_",df_so_concorridos[i,1],"_",df_so_concorridos[i,2]
+))))
+
+# colocar NA em df
+df[apply(df, 1, function(row) any(!is.finite(row))), ] <- NA
+
+# df -> df_meta_n CURSO ANO
+eval(parse(text=(paste0(
+  "df -> meta_n_",df_so_concorridos[i,1],"_",df_so_concorridos[i,2]
+))))
+} # fecha loop
---
+++ analysis_03_1_compara_conc_notas.R
@@ -0,0 +1,195 @@
+# # ==============================================================================
+# Arquivo: analysis_03_1_compara_conc_notas.R ESCREVENDO
+
+# Usa source analysis_03_0_compila_meta_n.R
+
+# Compara cinco modelos de concorrencia:
+# 1 = concorrencia separada (aka listas multiplas)
+# 2 = concorrencia concomitante, AC primeiro
+# 3 = concorrencia concomitante, cotas primeiro
+# 4 = concorrencia segundo Bó e Senkevics, 2023
+# 5 = concorrencia adaptado de Bó e Senkevics, 2023
+
+# Input:
+# meta_n_ CURSO ANO
+
+# Output:
+# meta_n_ CURSO ANO comparativo a c1
+# Gera dfs comparando_vagas
+# Exporta arquivo Excel com formatação condicional
+
+# Limitação:
+# Valores NA são exportados como células vazias
+
+# Modificado em 2024-01-03.
+# Autor: Mateus Silva Figueiredo
+
+# ==============================================================================
+# Preparação
+library(tidyverse)
+
+# ==============================================================================
+# Carregar dados a partir de analysis_03_0
+if (!exists("meta_n_DIREITO_SISU2019")) source("analysis_03_0_compila_meta_n.R") 
+# cria ~200 objetos
+
+# ==============================================================================
+# Iniciar alocação
+# definir inicio e fim, para depois rodar for loop
+
+# ------------------------------------------------------------------------------
+
+# apenas alguns conjuntos
+# inicio<-29; fim<-31
+
+# ==============================================================================
+
+# Exportar para Excel
+
+# Load the writexl package
+# install.packages("openxlsx")
+library(openxlsx)
+
+
+
+# ------------------------------------------------------------------------------
+
+# para um conjunto
+# inicio<-1; fim<-5
+
+# para todos os conjuntos:
+inicio<-1; fim<-nrow(df_so_concorridos)
+
+
+# ================ ABRE FOR LOOP ============================================
+
+# Create a new Excel workbook
+wb <- createWorkbook()
+
+for (i in inicio:fim){ # abre loop i, um para cada conjunto 
+
+# eval parse para carregar meta_n CURSO ANO em meta_n
+eval(parse(text=(paste0(
+"meta_n <- meta_n_",df_so_concorridos[i,1],"_",df_so_concorridos[i,2] 
+))))
+
+df_so_concorridos[i,]
+meta_n[2,]
+
+# Cria comparação entre c2:c5 - c1
+# Notas serão em relação a c1
+dif_med_c2<-(meta_n["mean_c2",]-meta_n["mean_c1",]) %>% round(3)
+dif_med_c3<-(meta_n["mean_c3",]-meta_n["mean_c1",]) %>% round(3)
+dif_med_c4<-(meta_n["mean_c4",]-meta_n["mean_c1",]) %>% round(3)
+dif_med_c5<-(meta_n["mean_c5",]-meta_n["mean_c1",]) %>% round(3)
+dif_min_c2<-meta_n["min_c2",]-meta_n["min_c1",] %>% round(3)
+dif_min_c3<-meta_n["min_c3",]-meta_n["min_c1",] %>% round(3)
+dif_min_c4<-meta_n["min_c4",]-meta_n["min_c1",] %>% round(3)
+dif_min_c5<-meta_n["min_c5",]-meta_n["min_c1",] %>% round(3)
+
+# para arredondar c1 também:
+meta_n["mean_c1",] <- meta_n["mean_c1",] %>% round(3)
+meta_n["min_c1",]  <- meta_n["min_c1",] %>% round(3)
+
+# cria dataframe dif_n com valor de c1 e todas as comparações
+dif_n <- rbind(
+  meta_n["mean_c1",],meta_n["min_c1",],
+  dif_med_c2, dif_min_c2,
+  dif_med_c3, dif_min_c3,
+  dif_med_c4, dif_min_c4,
+  dif_med_c5, dif_min_c5)
+
+# nome para linhas de dif_n
+rownames(dif_n) <- c("med_c1","min_c1",
+                     "dif med_c2","dif min_c2",
+                     "dif med_c3","dif min_c3",
+                     "dif med_c4","dif min_c4",
+                     "dif med_c5","dif min_c5")
+
+# eval parse para salvar dif_n em dif_n_ CURSO ANO
+eval(parse(text=(paste0(
+  "dif_n -> dif_n_",df_so_concorridos[i,1],"_",df_so_concorridos[i,2] 
+))))
+
+# produto final: dif_n_ CURSO ANO
+
+  # ------------------------------------------------------------------------------
+  # verificar resultados
+eval(parse(text=(paste0(
+  "print((dif_n_",df_so_concorridos[i,1],"_",df_so_concorridos[i,2],")[1,])" 
+))))
+
+# ==============================================================================
+# Exportação para Excel, feat. Chat GPT
+
+# ------------------------------------------------------
+# prepara palavras
+
+# obtém nome do curso
+curso <- df_so_concorridos[i,1]
+
+# caso curso tenha nome comprido, resumir assim:
+if(nchar(curso)>16){
+  curso<-paste0(substr(curso,1,5),
+                "_",
+                substr(curso, nchar(curso) - 9, nchar(curso)))
+}
+
+# obtém ano
+ano <- substr(df_so_concorridos[i,2],5,8)
+
+titulo_planilha <- paste0(curso,"_",ano)
+
+# Definir título da tabela e da planilha
+titulo_tabela <- paste0("Notas de ", df_so_concorridos[i,1]," ",df_so_concorridos[i,2])
+
+# ------------------------------------------------------
+# cria planilha e preenche
+
+# Add data to separate sheets with titles
+addWorksheet(wb, titulo_planilha)
+writeData(wb, titulo_planilha, titulo_tabela, startCol = 1, startRow = 1)
+writeData(wb, titulo_planilha, dif_n, startCol = 1, startRow = 2, rowNames = TRUE)
+
+# Formatação condicional nas linhas de c2 até c5
+# min_value <- min(dif_n[3:10,])-1
+# max_value <- max(dif_n[3:10,])+1
+
+negStyle <- createStyle(fontColour = "black", bgFill = "pink")
+posStyle <- createStyle(fontColour = "black", bgFill = "lightgreen")
+# 
+conditionalFormatting(wb, sheet = titulo_planilha,
+                      type = "expression",
+                      rule = "<0",
+                      style = negStyle,
+                      rows = 5:12, cols = 2:12)
+
+conditionalFormatting(wb, sheet = titulo_planilha,
+                      type = "expression",
+                      rule = ">0",
+                      style = posStyle,
+                      rows = 5:12, cols = 2:12)
+
+} # fim do loop i, de cada conjunto
+
+# ==============================================================================
+
+# Save the Excel workbook
+if(T){
+tempo_atual <- format(Sys.time(), "%Y-%m-%d-%H-%M-%S")
+save_path <- paste0("output_analysis_03_1_comp_notas_", tempo_atual, ".xlsx")
+saveWorkbook(wb, save_path, overwrite = TRUE)
+}
+
+# ==============================================================================
+# Limpeza
+if(F){
+  # remover objetos 
+  rm(list = ls(pattern = "^analise_"))
+  rm(list = ls(pattern = "^meta_"))
+  rm(list = ls(pattern = "^nvagas_"))
+  rm(df)
+  rm(list = ls(pattern = "^dados_")) # remover objetos dados_
+  #  rm("candidatos")
+}
+
